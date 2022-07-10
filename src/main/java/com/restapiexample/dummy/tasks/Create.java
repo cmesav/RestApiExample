77eb7a5a2da7a553a8417ae6f35dce41199b9c57
Cristian Mesa <mesa.cristian@gmail.com>
2022-07-10 18:56:04 -0500

    se modifica post, se elimina clase headers

--- src/main/java/com/restapiexample/dummy/tasks/Create.java
+++ src/main/java/com/restapiexample/dummy/tasks/Create.java
@@ -1,7 +1,6 @@
 package com.restapiexample.dummy.tasks;
 
 import com.restapiexample.dummy.models.Data;
-import com.restapiexample.dummy.utils.Headers;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
@@ -9,6 +8,8 @@ import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.rest.interactions.Post;
 import net.thucydides.core.annotations.Step;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class Create implements Task {
@@ -27,7 +28,7 @@ public class Create implements Task {
         Logger logger = Logger.getLogger(Obtain.class.getName());
 
         actor.attemptsTo(Post.to(path).with(requestSpecification -> requestSpecification
-                .headers(Headers.build()).body(data).relaxedHTTPSValidation()));
+                .headers(heads()).body(data).relaxedHTTPSValidation()));
 
         logger.info("Create employee response body");
         SerenityRest.lastResponse().prettyPrint();
@@ -36,5 +37,17 @@ public class Create implements Task {
 
     public static Create employee(Data data, String path){
         return Tasks.instrumented(Create.class, data, path);
+    }
+
+    private static Map<String, Object> heads() {
+
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("Accept", "*/*");
+        headers.put("Cache-Control","no-cache");
+        headers.put("Host","dummy.restapiexample.com");
+        headers.put("Connection","keep-alive");
+        headers.put("Content-Type","application/json");
+
+        return headers;
     }
 }
